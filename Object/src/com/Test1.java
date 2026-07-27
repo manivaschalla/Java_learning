7b589b8c549caf48fc1fa30c09ba3a5680126ee8
Manivas <manivaschalla@gmail.com>
2026-07-27 18:00:23 +0530

    Objects in java

---
+++ Object/src/com/Test1.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Test1 extends objClass{
+	public static void main(String[] args) {
+		
+		objClass oc = new Test1();
+		
+		oc.start();
+		oc.id =1;
+		System.out.println(oc.id);
+		System.out.println(oc.wish);
+		
+	}
+}
---
+++ Object/src/com/objClass.java
@@ -0,0 +1,13 @@
+package com;
+
+public class objClass {
+	
+	int id;
+	
+	String wish = "Hello";
+	
+	void start() {
+		System.out.println("Starting");
+	}
+
+}
